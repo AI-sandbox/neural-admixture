db62e679e05099e0b158823155b36ec87ab01289
Albert Dominguez <albertadm99@gmail.com>
2021-03-04 23:42:00 +0000

    Fix data path

--- enqueue.sh
+++ enqueue.sh
@@ -6,4 +6,4 @@
 #SBATCH --time=12:00:00
 source ~/venv/deep_genomics/bin/activate
 cd ~/scripts/src
-python3 launch_fit.py --wandb_log 1 --display_logs 0 --batchnorm 1 --dropout 0.25 --l2_penalty 0 --lr 0.001 --bs 200 --k 7 --epochs 500 --lambda_l0 0 --decoder_init minibatch_kmeans_logit --loss mse --mask_frac 0.6 --optimizer adam --save_every 10 --save_dir ../../outputs --window_size 100K --deep_encoder 0
+python3 launch_fit.py --wandb_log 0 --display_logs 0 --batchnorm 1 --dropout 0.25 --l2_penalty 0 --lr 0.001 --bs 200 --k 7 --epochs 500 --lambda_l0 0 --decoder_init minibatch_kmeans_logit --loss mse --mask_frac 0.6 --optimizer adam --save_every 10 --save_dir ../../outputs --window_size 100K --deep_encoder 0
