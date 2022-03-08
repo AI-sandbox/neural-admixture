1c9583f300c2cc863b061de8a8e9d43481659fb1
Albert Dominguez <albertadm99@gmail.com>
2022-03-08 16:51:50 +0100

    [WIP] Mean imputation. Allow missing SNPs in inference mode.

--- neural_admixture/src/snp_reader.py
+++ neural_admixture/src/snp_reader.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import numpy as np
 import sys
@@ -48,7 +49,7 @@ class SNPReader:
             return npy/2
         return npy.sum(axis=2)/2
     
-    def read_data(self, file):
+    def _read_data(self, file):
         if file.endswith('.vcf') or file.endswith('.vcf.gz'):
             G = self._read_vcf(file)
         elif file.endswith('.h5') or file.endswith('.hdf5'):
@@ -62,5 +63,51 @@ class SNPReader:
         else:
             log.error('Invalid format. Unrecognized file format. Make sure file ends with .vcf | .vcf.gz | .bed | .pgen | .h5 | .hdf5 | .npy')
             sys.exit(1)
-        assert int(G.min()) == 0 and int(G.max()) == 1, 'Only biallelic SNPs are supported. Please make sure multiallelic sites have been removed.'
+        assert int(G.max()) == 1, 'Only biallelic SNPs are supported. Please make sure multiallelic sites have been removed.'
         return G if np.mean(G) < 0.5 else 1-G
+    
+    def process_data(self, G, variant_ids, save_config_path=None, train_config=None):
+        # TODO: unittest this function
+        na_mask = G < 0
+        if len(na_mask) > 0:
+            G[na_mask] = np.nan
+
+        # TODO: there's probably a (faster) npy-friendly way to do this
+        if train_config is not None:
+            log.info('Computing variant-wise mean...')
+            snp_means = np.nanmean(G, axis=0)
+            if len(na_mask) > 0:
+                log.info('Running mean imputation on missing values...')
+                idxs = np.where(na_mask)
+                idxs = [(idxs[0][i], idxs[1][i]) for i in range(len(idxs[0]))]
+                for i, j in idxs:
+                    G[i, j] = snp_means[j]
+            snp_info = {variant_ids[i]: {'idx': i, 'mean': snp_means[i]} for i in range(len(variant_ids))}
+            log.info('Storing variants information...')
+            with open(save_config_path, 'w') as fb:
+                json.dump(snp_info, fb)
+            return G
+        else:
+            processed_G = np.empty(G.shape) # This array will be filled with proper order
+            processed_idxs = set()
+            log.info('Matching new data with training configuration...')
+            for old_idx, variant in enumerate(variant_ids):
+                if variant in train_config.keys(): # Variant in inference data was also in training data
+                    new_idx, mean = train_config[variant]['idxs'], float(train_config[variant]['mean'])
+                    processed_G[:, new_idx] = np.nan_to_num(G[:, old_idx], nan=mean) # Put calldata in the correct position and fill NA's with training data means
+                    processed_idxs.add(new_idx)
+            if len(processed_idxs) != processed_G.shape[1]:
+                # Some training SNPs were not found in new data. Use their training means instead.
+                log.warn(f'{processed_G.shape[0]-len(processed_idxs)} variants used in train are missing from the data. Will use mean values instead.')
+                for variant_info in train_config.values():
+                    if variant_info['idx'] not in processed_idxs:
+                       processed_G[:, variant_info['idx']] = variant_info['mean']
+            G = processed_G
+            return G
+        
+    
+    def read_and_process_data(self):
+        # TODO: call process data too, check arguments
+        G = self._read_data()
+        return G
+
