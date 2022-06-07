171b5d831dfc3cb86332f215bc5e797147631633
AlbertDominguez <adomi@stanford.edu>
2022-06-07 16:03:15 -0700

    Fix parse issues in certain distributions

--- neural_admixture/entry.py
+++ neural_admixture/entry.py
@@ -5,12 +5,13 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger(__name__)
 
 def main():
-    assert len(sys.argv) > 2, 'Please provide either the argument "train" or "infer" to choose running mode.'
+    arg_list = tuple(sys.argv)
+    assert len(arg_list) > 1, 'Please provide either the argument "train" or "infer" to choose running mode.'
     if sys.argv[1] == 'train':
         from src import train
-        sys.exit(train.main(sys.argv[2:]))
+        sys.exit(train.main(arg_list[2:]))
     if sys.argv[1] == 'infer':
         from src import inference
-        sys.exit(inference.main(sys.argv[2:]))
-    log.error(f'Invalid argument {sys.argv[1]}. Please run either "neural-admixture train" or "neural-admixture infer"')
+        sys.exit(inference.main(arg_list[2:]))
+    log.error(f'Invalid argument {arg_list[1]}. Please run either "neural-admixture train" or "neural-admixture infer"')
     sys.exit(1)
