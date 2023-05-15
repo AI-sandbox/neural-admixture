7005a08d2b6ba6e9cff95998eb73fe1cac13f595
AlbertDominguez <adomi@stanford.edu>
2023-05-15 23:36:05 +0200

    docstring

--- neural_admixture/model/neural_admixture.py
+++ neural_admixture/model/neural_admixture.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 import wandb
 from pathlib import Path
 from tqdm.auto import tqdm
+from typing import Iterable, Optional, Tuple, Union
 
 from .modules import NeuralDecoder, NeuralEncoder, ZeroOneClipper
 
@@ -17,9 +18,22 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 log = logging.getLogger(__name__)
 
 class NeuralAdmixture(nn.Module):
-    def __init__(self, ks, num_features, encoder_activation=nn.GELU(),
-                 P_init=None, lambda_l2=0.0005, hidden_size=64,
-                 freeze_decoder=False, supervised=False, supervised_loss_weight=0.05):
+    """Instantiate Neural ADMIXTURE model
+
+    Args:
+        ks (Iterable[int]): different numbers of populations to use. If the list contains more than one value, the multi-head version will run.
+        num_features (int): number of SNPs used to train the network.
+        encoder_activation (nn.Module, optional): activation function used in the encoder. Defaults to nn.GELU().
+        P_init (Optional[torch.Tensor], optional): if provided, corresponds to initialization weights as returned by one of the initialization functions. Defaults to None.
+        lambda_l2 (float, optional): L2 regularization strength in the encoder. Smaller values will give quasi-binary predictions. Defaults to 5e-4.
+        hidden_size (int, optional): number of neurons in the first linear layer. Defaults to 64.
+        freeze_decoder (bool, optional): if set to True, the decoder weights are frozen (useful for reusing other results). Defaults to False.
+        supervised (bool, optional): if set to True, will run in supervised mode. Defaults to False.
+        supervised_loss_weight (float, optional): weight given to the supervised loss term. Only applied if running in supervised mode. Defaults to 0.05.
+    """
+    def __init__(self, ks: Iterable[int], num_features: int, encoder_activation: nn.Module=nn.GELU(),
+                 P_init: Optional[torch.Tensor]=None, lambda_l2: float=5e-4, hidden_size: int=64,
+                 freeze_decoder: bool=False, supervised: bool=False, supervised_loss_weight: float=0.05) -> None:
         super().__init__()
         self.ks = ks
         self.num_features = num_features
@@ -40,7 +54,18 @@ class NeuralAdmixture(nn.Module):
         self.clipper = ZeroOneClipper()
         self.decoders.decoders.apply(self.clipper)
 
-    def forward(self, X, only_assignments=False, only_hidden_states=False):
+    def forward(self, X: torch.Tensor, only_assignments: bool=False,
+                only_hidden_states: bool=False) -> Union[torch.Tensor, Iterable[torch.Tensor], Tuple[torch.Tensor, torch.Tensor]]:
+        """Forward pass of the model
+
+        Args:
+            X (torch.Tensor): _description_
+            only_assignments (bool, optional): if set to True, only return the Q values (used in inference mode for speeding up). Defaults to False.
+            only_hidden_states (bool, optional): if set to True, only return logits of the Q values. Defaults to False.
+
+        Returns:
+            Union[torch.Tensor, Iterable[torch.Tensor], Tuple[torch.Tensor, torch.Tensor]]: SNP reconstruction and Q values (if last two parameters are set to False), otherwise only Q values or logits of Q values.
+        """
         X = self.batch_norm(X)
         enc = self.common_encoder(X)
         del X
@@ -53,11 +78,34 @@ class NeuralAdmixture(nn.Module):
         del enc
         return self.decoders(probs), probs
 
-    def launch_training(self, trX, optimizer, loss_f, num_epochs,
-                        device, batch_size=0, valX=None,
-                        save_every=10, save_path='../outputs/model.pt',
-                        trY=None, valY=None, seed=42, shuffle=True, log_to_wandb=False,
-                        tol=1e-5):
+    def launch_training(self, trX: da.core.Array, optimizer: torch.optim.Optimizer,
+                        loss_f: torch.nn.modules.loss._Loss, num_epochs: int,
+                        device: torch.device, batch_size: int=0, valX: Optional[da.core.Array]=None,
+                        save_every: int=10, save_path: str='../outputs/model.pt',
+                        trY: Optional[Iterable[str]]=None, valY: Optional[Iterable[str]]=None, seed: int=42,
+                        shuffle: bool=True, log_to_wandb: bool=False, tol: float=1e-5) -> int:
+        """Launch training pipeline of the model
+
+        Args:
+            trX (da.core.Array): training data matrix
+            optimizer (torch.optim.Optimizer): loaded optimizer to use
+            loss_f (torch.nn.modules.loss._Loss): instantiated loss function to use
+            num_epochs (int): maximum number of epochs. Note that the training might stop earlier if the loss is in a plateau.
+            device (torch.device): device to use for training
+            batch_size (int, optional): batch size. If 0, will use all the training data in a single batch. Defaults to 0.
+            valX (Optional[da.core.Array], optional): _description_. Defaults to None.
+            save_every (int, optional): save a checkpoint after this number of epochs. Defaults to 10.
+            save_path (str, optional): output path of the trained model. Defaults to '../outputs/model.pt'.
+            trY (Optional[Iterable[str]], optional): list of training populations per sample. Defaults to None.
+            valY (Optional[Iterable[str]], optional): list of validation populations per sample. Defaults to None.
+            seed (int, optional): seed for RNG. Defaults to 42.
+            shuffle (bool, optional): whether to shuffle the samples at every epoch. Defaults to True.
+            log_to_wandb (bool, optional): whether to log training to wandb. Defaults to False.
+            tol (float, optional): tolerance for early stopping. Training will stop if decrease in loss is smaller than this value. Defaults to 1e-5.
+
+        Returns:
+            int: number of actual training epochs ran
+        """
         random.seed(seed)
         loss_f_supervised, trY_num, valY_num = None, None, None
         tr_losses, val_losses = [], []
@@ -96,12 +144,32 @@ class NeuralAdmixture(nn.Module):
         log.info(f'Max epochs reached. Stopping fit...')
         return ep+1
 
-    def _get_encoder_norm(self, p=2):
+    def _get_encoder_norm(self, p: int=2) -> torch.Tensor:
+        """Retrieve the sum of the norm of the encoder parameters (for regularization purposes)
+
+        Args:
+            p (int, optional): norm to retrieve. Defaults to 2.
+
+        Returns:
+            torch.Tensor: sum of norms of the encoder parameters
+        """
         shared_params = torch.cat([x.view(-1) for x in self.common_encoder.parameters()])
         multihead_params = torch.cat([x.view(-1) for x in self.multihead_encoder.parameters()])
         return torch.norm(shared_params, p)+torch.norm(multihead_params, p)
 
-    def _run_step(self, X, optimizer, loss_f, loss_f_supervised=None, y=None):
+    def _run_step(self, X: torch.Tensor, optimizer: torch.optim.Optimizer, loss_f: torch.nn.modules.loss._Loss,
+                  loss_f_supervised=Optional[torch.nn.modules.loss._Loss], y=Optional[Iterable[str]]):
+        """Run a single optimization step
+        Args:
+            X (torch.Tensor): mini-batch of data
+            optimizer (torch.optim.Optimizer): loaded optimizer to use
+            loss_f (torch.nn.modules.loss._Loss): instantiated loss function to use
+            loss_f_supervied (Optional[torch.nn.modules.loss._Loss]): instantiated supervied loss function to use
+            y (Optional[Iterable[str]], optional): list of training populations per sample. Defaults to None.
+
+        Returns:
+            float: loss value of the mini-batch
+        """
         optimizer.zero_grad(set_to_none=True)
         recs, hid_states = self(X)
         loss = sum((loss_f(rec, X) for rec in recs))
@@ -116,7 +184,21 @@ class NeuralAdmixture(nn.Module):
         self.decoders.decoders.apply(self.clipper)
         return loss.item()
     
-    def _validate(self, valX, loss_f, batch_size, device, loss_f_supervised=None, y=None):
+    def _validate(self, valX: da.core.Array, loss_f: torch.nn.modules.loss._Loss, batch_size: int, device: torch.device,
+                  loss_f_supervised: Optional[torch.nn.modules.loss._Loss]=None, y: Optional[Iterable[str]]=None) -> float:
+        """_summary_
+
+        Args:
+            valX (da.core.Array): validation data matrix
+            loss_f (torch.nn.modules.loss._Loss): instantiated loss function to use
+            batch_size (int): batch size. Note that the last batch might be smaller.
+            device (torch.device): device to use for training
+            loss_f_supervied (Optional[torch.nn.modules.loss._Loss]): instantiated supervied loss function to use
+            y (Optional[Iterable[str]], optional): list of training populations per sample. Defaults to None.
+
+        Returns:
+            float: loss value of the validation set
+        """
         acum_val_loss = 0
         with torch.inference_mode():
             for X, y_b in self.batch_generator(valX, batch_size, y=y if loss_f_supervised is not None else None):
