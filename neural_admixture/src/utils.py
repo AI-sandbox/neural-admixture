3136d9236c6c728996c8b6f746acba812b1483f6
JOAN SAURINA RICÓS <joan.saurina@estudiantat.upc.edu>
2025-01-22 11:56:50 +0100

    'rmsdnorm'

--- neural_admixture/src/utils.py
+++ neural_admixture/src/utils.py
@@ -26,8 +26,8 @@ def parse_train_args(argv: List[str]):
                                            description='Rapid population clustering with autoencoders - training mode',
                                            config_file_parser_class=configargparse.YAMLConfigFileParser)
     
-    parser.add_argument('--epochs_P1', required=False, type=int, default=50, help='Maximum number of epochs for phase 1.')
-    parser.add_argument('--epochs_P2', required=False, type=int, default=500, help='Maximum number of epochs for phase 2.')
+    parser.add_argument('--epochs_P1', required=False, type=int, default=5, help='Maximum number of epochs for phase 1.')
+    parser.add_argument('--epochs_P2', required=False, type=int, default=250, help='Maximum number of epochs for phase 2.')
     parser.add_argument('--batch_size_P1', required=False, default=400, type=int, help='Batch size for phase 1.')
     parser.add_argument('--batch_size_P2', required=False, default=800, type=int, help='Batch size for phase 2.')
     
